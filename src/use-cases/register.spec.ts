865047f3b6d20271a87c4b6e1fed410810082324
Joao Victor Souza <jvsouzasantos@id.uff.br>
2024-04-03 22:37:44 -0300

    test: impedir a criação de 2 usuario com o mesmo email

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistError } from './errors/user-already-exists-error'
 
 describe('Register User Case', () => {
   it('should hash user password upon registration', async () => {
@@ -20,5 +21,26 @@ describe('Register User Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '12345678',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistError)
   })
 })
